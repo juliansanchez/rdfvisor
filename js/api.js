2ee15e947eb898d35a252e07702c354b0694930e
Julián Sánchez <eljovenjuli@hotmail.com>
2018-12-01 15:07:06 +0100

    AddSearchBooksAuthors

--- js/api.js
+++ js/api.js
@@ -1,4 +1,7 @@
 
+var portada="img/default.png";
+
+
 function mostrarBasico(){
   var endpointUrl = 'https://query.wikidata.org/sparql',
     sparqlQuery = "SELECT distinct ?item ?itemLabel ?itemDescription ?article ?image ?countryLabel ?categoryLabel ?movementLabel ?movement WHERE{\n" +
@@ -81,7 +84,7 @@ function mostrarLibros(){
 
 $.ajax( endpointUrl, settings ).then( function ( data ) {
     // $( 'body' ).append( ( $('<pre>').text( JSON.stringify( data) ) ) );
-    console.log( data );
+    // console.log( data );
     // console.log(data.results);
     // console.log(data.results.bindings);
     // console.log(data.results.bindings[0].image.value);
@@ -101,94 +104,97 @@ $.ajax( endpointUrl, settings ).then( function ( data ) {
               }
             }
           }
-
         }
       }
-
     }
   } );
 }
 
 
-function buscador(){
-  $(function() {
 
+
+$(function() {
+  // click ajax call
+  document.getElementById("cont").innerHTML +="";
+  $("#searchAutor").on("click", function() {
+    var searchTermAutor = $("#searchTermAutor").val();
+    searchTermAutor= searchTermAutor.toUpperCase();
 
     var endpointUrl = 'https://query.wikidata.org/sparql',
-      sparqlQuery = "SELECT ?bookText ?bookTextLabel ?fechaPublicado ?image ?autor ?autorLabel\n" +
-          "WHERE {\n" +
-          "  \n" +
-          "    {?bookText wdt:P31 wd:Q83790 . ?bookText wdt:P407 wd:Q1321 . ?bookText wdt:P577 ?fechaPublicado .  } #libro de texto\n" +
-          "   UNION\n" +
-          "    {?bookText wdt:P31 wd:Q571 . ?bookText wdt:P407 wd:Q1321 . ?bookText wdt:P577 ?fechaPublicado .} #libro impreso\n" +
-          "   UNION\n" +
-          "    {?bookText wdt:P31 wd:Q8261 . ?bookText wdt:P407 wd:Q1321 . ?bookText wdt:P577 ?fechaPublicado .} #novela\n" +
-          "   UNION\n" +
-          "    {?bookText wdt:P31 wd:Q35760 . ?bookText wdt:P407 wd:Q1321 . ?bookText wdt:P577 ?fechaPublicado .} #ensayo #idioma de la obra spaña\n" +
-          "   UNION\n" +
-          "    {?bookText wdt:P31 wd:Q37484 . ?bookText wdt:P407 wd:Q1321 . ?bookText wdt:P577 ?fechaPublicado .} #poema épico\n" +
-          "   UNION\n" +
-          "    {?bookText wdt:P31 wd:Q7725634 . ?bookText wdt:P407 wd:Q1321 . ?bookText wdt:P577 ?fechaPublicado .} #obra literaria\n" +
-          "   UNION\n" +
-          "    {?bookText wdt:P31 wd:Q25379 . ?bookText wdt:P407 wd:Q1321 . ?bookText wdt:P577 ?fechaPublicado .} #play\n" +
-          "   UNION\n" +
-          "    {?bookText wdt:P31 wd:Q25379 . ?bookText wdt:P407 wd:Q1088025 . ?bookText wdt:P577 ?fechaPublicado .} #play español antiguo\n" +
-          "  UNION\n" +
-          "    {?bookText wdt:P31 wd:Q25379 . ?bookText wdt:P407 wd:Q397 . ?bookText wdt:P577 ?fechaPublicado .} #play latin\n" +
-          "  \n" +
-          "  OPTIONAL {?bookText wdt:P577 ?fechaPublicado . ?bookText wdt:P18 ?image . ?bookText wdt:P50 ?autor .} \n" +
-          "\n" +
-          "\n" +
-          "  \n" +
-          "  #fecha del primer y último libro publicado\n" +
-          "  filter (?fechaPublicado > \"1492-01-01\"^^xsd:dateTime && ?fechaPublicado < \"1681-05-26\"^^xsd:dateTime) \n" +
-          "\n" +
-          "\n" +
-          "  \n" +
-          "  SERVICE wikibase:label { bd:serviceParam wikibase:language \"[AUTO_LANGUAGE],es\". }\n" +
-          "}\n" +
-          "\n" +
-          "ORDER BY ASC(?fechaPublicado)\n" +
-          "",
+    sparqlQuery = "SELECT DISTINCT ?bookText ?bookTextLabel ?fechaPublicado ?image ?autor ?autorLabel ?genreLabel\n" +
+        "WHERE {\n" +
+        "  \n" +
+        "    {?bookText wdt:P31 wd:Q83790 . ?bookText wdt:P407 wd:Q1321 . ?bookText wdt:P577 ?fechaPublicado .  } #libro de texto\n" +
+        "   UNION\n" +
+        "    {?bookText wdt:P31 wd:Q571 . ?bookText wdt:P407 wd:Q1321 . ?bookText wdt:P577 ?fechaPublicado .} #libro impreso\n" +
+        "   UNION\n" +
+        "    {?bookText wdt:P31 wd:Q8261 . ?bookText wdt:P407 wd:Q1321 . ?bookText wdt:P577 ?fechaPublicado .} #novela\n" +
+        "   UNION\n" +
+        "    {?bookText wdt:P31 wd:Q35760 . ?bookText wdt:P407 wd:Q1321 . ?bookText wdt:P577 ?fechaPublicado .} #ensayo #idioma de la obra spaña\n" +
+        "   UNION\n" +
+        "    {?bookText wdt:P31 wd:Q37484 . ?bookText wdt:P407 wd:Q1321 . ?bookText wdt:P577 ?fechaPublicado .} #poema épico\n" +
+        "   UNION\n" +
+        "    {?bookText wdt:P31 wd:Q7725634 . ?bookText wdt:P407 wd:Q1321 . ?bookText wdt:P577 ?fechaPublicado .} #obra literaria\n" +
+        "   UNION\n" +
+        "    {?bookText wdt:P31 wd:Q25379 . ?bookText wdt:P407 wd:Q1321 . ?bookText wdt:P577 ?fechaPublicado .} #play\n" +
+        "   UNION\n" +
+        "    {?bookText wdt:P31 wd:Q25379 . ?bookText wdt:P407 wd:Q1088025 . ?bookText wdt:P577 ?fechaPublicado .} #play español antiguo\n" +
+        "  UNION\n" +
+        "    {?bookText wdt:P31 wd:Q25379 . ?bookText wdt:P407 wd:Q397 . ?bookText wdt:P577 ?fechaPublicado .} #play latin\n" +
+        "  \n" +
+        "  OPTIONAL {?bookText wdt:P577 ?fechaPublicado . ?bookText wdt:P18 ?image . ?bookText wdt:P50 ?autor . ?bookText wdt:P136 ?genre .} \n" +
+        "\n" +
+        "\n" +
+        "  \n" +
+        "  #fecha del primer y último libro publicado\n" +
+        "  filter (?fechaPublicado > \"1492-01-01\"^^xsd:dateTime && ?fechaPublicado < \"1681-05-26\"^^xsd:dateTime) \n" +
+        "\n" +
+        "\n" +
+        "  \n" +
+        "  SERVICE wikibase:label { bd:serviceParam wikibase:language \"[AUTO_LANGUAGE],es\". }\n" +
+        "}\n" +
+        "\n" +
+        "ORDER BY ASC(?fechaPublicado)\n" +
+        "",
       settings = {
           headers: { Accept: 'application/sparql-results+json' },
           data: { query: sparqlQuery }
       };
 
   $.ajax( endpointUrl, settings ).then( function ( data ) {
+    document.getElementById("cont").innerHTML="";
       // $( 'body' ).append( ( $('<pre>').text( JSON.stringify( data) ) ) );
-      console.log( data );
+      // console.log( data );
       // console.log(data.results);
       // console.log(data.results.bindings);
       // console.log(data.results.bindings[0].image.value);
+      if (data.results.bindings != null) {
+        for (var i in data.results.bindings) {
+          if (data.results.bindings[i] != null) {
+            for (var j in data.results.bindings[i]) {
+              if (data.results.bindings[i][j] != null) {
+                // console.log(data.results.bindings[i][j]);
+                if  (j!=null && j == "image") {
+                  portada=data.results.bindings[i][j].value;
+                  // console.log(data.results.bindings[i][j].value);
+                }
+
+                if (j !=null && j == "bookTextLabel" && data.results.bindings[i][j].value.toUpperCase().includes(searchTermAutor)
+                    || j !=null && j == "autorLabel" && data.results.bindings[i][j].value.toUpperCase().includes(searchTermAutor)) {
+
+                  document.getElementById("cont").innerHTML +="<h4>"+data.results.bindings[i].bookTextLabel.value+"</h4>";
+                  document.getElementById("cont").innerHTML +="<img class='portada' src='"+portada+"'>";
+                  document.getElementById("cont").innerHTML +="<h5>"+data.results.bindings[i].autorLabel.value+"</h5>";
+                  document.getElementById("cont").innerHTML +="<h6>"+data.results.bindings[i].genreLabel.value.toUpperCase()+"</h6>";
+
+                }
+
+              }
+            }
+          }
+        }
+      }
 
     } );
-
-
-
-    // enter
-      $("#searchTerm2").keypress(function(e){
-      	if(e.keyCode===13){
-      		var searchTerm = $("#searchTerm2").val();
-          var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+ searchTerm +"&limit=5&format=json&callback=?";
-
-      	}
-      });
-
-
-    // click ajax call
-    $("#search2").on("click", function() {
-  	var searchTerm = $("#searchTerm2").val();
-    if (searchTerm=="") {
-      // swal("No has escrito nada payaso!","");
-      $.alert({
-      title: 'Campo vacío!',
-      content: 'Introduce un texto!',
-      });
-    }
-  	var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+ searchTerm +"&limit=10&format=json&callback=?";
-
-    });
   });
-
-}
+});
