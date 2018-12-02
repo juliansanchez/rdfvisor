bbe7f6e987fe192be692a6f1bfc5e4caded93828
Julián Sánchez <eljovenjuli@hotmail.com>
2018-12-02 17:09:45 +0100

    ShowBooks

--- js/api.js
+++ js/api.js
@@ -1,6 +1,12 @@
 
 var portada="img/default.png";
 
+/* Primera letra en Mayusculas */
+function MaysPrimera(string){
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+/* INFO HOME PAGE */
 function mostrarBasico(){
   var endpointUrl = 'https://query.wikidata.org/sparql',
     sparqlQuery = "SELECT distinct ?item ?itemLabel ?itemDescription ?article ?image ?countryLabel ?categoryLabel ?movementLabel ?movement WHERE{\n" +
@@ -39,94 +45,16 @@ function mostrarBasico(){
     document.getElementById("infoBasic").innerHTML += "<p><a href="+article+" target='_blank'</a>Wikipedia</p>";
     document.getElementById("infoBasic").innerHTML += "<p><a href="+item+" target='_blank'</a>"+categoryLabel+"</p>";
     document.getElementById("infoBasic").innerHTML += "<p>"+countryLabel+"</p>";
-    document.getElementById("infoBasic").innerHTML += "<p>Descripción: "+itemDescription+"</p>";
-    document.getElementById("infoBasic").innerHTML += "<p>Movimiento Literario: <a href="+movement+" target='_blank'</a>"+movementLabel+"</p>";
+    document.getElementById("infoBasic").innerHTML += "<p>Descripción: "+MaysPrimera(itemDescription)+"</p>";
+    document.getElementById("infoBasic").innerHTML += "<p>Movimiento Literario: <a href="+movement+" target='_blank'</a>"+MaysPrimera(movementLabel)+"</p>";
 
   });
 }
 
 function mostrarLibros(){
+
   var endpointUrl = 'https://query.wikidata.org/sparql',
-    sparqlQuery = "SELECT ?bookText ?bookTextLabel ?fechaPublicado ?image ?autor ?autorLabel\n" +
-        "WHERE {\n" +
-        "  \n" +
-        "    {?bookText wdt:P31 wd:Q83790 . ?bookText wdt:P407 wd:Q1321 . ?bookText wdt:P577 ?fechaPublicado .  } #libro de texto\n" +
-        "   UNION\n" +
-        "    {?bookText wdt:P31 wd:Q571 . ?bookText wdt:P407 wd:Q1321 . ?bookText wdt:P577 ?fechaPublicado .} #libro impreso\n" +
-        "   UNION\n" +
-        "    {?bookText wdt:P31 wd:Q8261 . ?bookText wdt:P407 wd:Q1321 . ?bookText wdt:P577 ?fechaPublicado .} #novela\n" +
-        "   UNION\n" +
-        "    {?bookText wdt:P31 wd:Q35760 . ?bookText wdt:P407 wd:Q1321 . ?bookText wdt:P577 ?fechaPublicado .} #ensayo #idioma de la obra spaña\n" +
-        "   UNION\n" +
-        "    {?bookText wdt:P31 wd:Q37484 . ?bookText wdt:P407 wd:Q1321 . ?bookText wdt:P577 ?fechaPublicado .} #poema épico\n" +
-        "   UNION\n" +
-        "    {?bookText wdt:P31 wd:Q7725634 . ?bookText wdt:P407 wd:Q1321 . ?bookText wdt:P577 ?fechaPublicado .} #obra literaria\n" +
-        "   UNION\n" +
-        "    {?bookText wdt:P31 wd:Q25379 . ?bookText wdt:P407 wd:Q1321 . ?bookText wdt:P577 ?fechaPublicado .} #play\n" +
-        "   UNION\n" +
-        "    {?bookText wdt:P31 wd:Q25379 . ?bookText wdt:P407 wd:Q1088025 . ?bookText wdt:P577 ?fechaPublicado .} #play español antiguo\n" +
-        "  UNION\n" +
-        "    {?bookText wdt:P31 wd:Q25379 . ?bookText wdt:P407 wd:Q397 . ?bookText wdt:P577 ?fechaPublicado .} #play latin\n" +
-        "  \n" +
-        "  OPTIONAL {?bookText wdt:P577 ?fechaPublicado . ?bookText wdt:P18 ?image . ?bookText wdt:P50 ?autor .} \n" +
-        "\n" +
-        "\n" +
-        "  \n" +
-        "  #fecha del primer y último libro publicado\n" +
-        "  filter (?fechaPublicado > \"1492-01-01\"^^xsd:dateTime && ?fechaPublicado < \"1681-05-26\"^^xsd:dateTime) \n" +
-        "\n" +
-        "\n" +
-        "  \n" +
-        "  SERVICE wikibase:label { bd:serviceParam wikibase:language \"[AUTO_LANGUAGE],es\". }\n" +
-        "}\n" +
-        "\n" +
-        "ORDER BY ASC(?fechaPublicado)\n" +
-        "",
-    settings = {
-        headers: { Accept: 'application/sparql-results+json' },
-        data: { query: sparqlQuery }
-    };
-
-$.ajax( endpointUrl, settings ).then( function ( data ) {
-    // $( 'body' ).append( ( $('<pre>').text( JSON.stringify( data) ) ) );
-    // console.log( data );
-    // console.log(data.results);
-    // console.log(data.results.bindings);
-    // console.log(data.results.bindings[0].image.value);
-    if (data.results.bindings != null) {
-      for (var i in data.results.bindings) {
-        // console.log(data.results.bindings[i]);
-        if (data.results.bindings[i] != null) {
-          for (var j in data.results.bindings[i]) {
-            if (data.results.bindings[i][j] != null) {
-              if (j !=null && j == "image") {
-                document.getElementById("libros").innerHTML +="<div class='col-md-4'><img class='portada'src='"+data.results.bindings[i][j].value+"'></div>";
-                // document.getElementById("libros").innerHTML +="<h6>"+j+"</h6><p>"+data.results.bindings[i][j].value+"</p>";
-              }else if(j!="image") {
-                // document.getElementById("libros").innerHTML +="<img class='portada'src='img/default.png'>";
-                document.getElementById("libros").innerHTML +="<div class='col-md-4'><p style='strong'></p><span>"+data.results.bindings[i][j].value+"</span></div>";
-
-              }
-            }
-          }
-        }
-      }
-    }
-  } );
-}
-
-
-
-$(function() {
-  var elemento = [];
-  // click ajax call
-  $("#searchAutor").on("click", function() {
-    document.getElementById("cont").innerHTML = "";
-    var searchTermAutor = $("#searchTermAutor").val();
-    var searchTermAutorMAY= searchTermAutor.toUpperCase();
-
-    var endpointUrl = 'https://query.wikidata.org/sparql',
-    sparqlQuery = "SELECT DISTINCT ?bookText ?bookTextLabel ?image ?autor ?autorLabel ?fechaPublicado ?genre ?genreLabel ?idioma ?idiomaLabel\n" +
+    sparqlQuery = "SELECT DISTINCT ?bookText ?bookTextLabel ?bookTextDescription ?image ?autor ?autorLabel ?fechaPublicado ?genre ?genreLabel ?idioma ?idiomaLabel\n" +
         "WHERE {\n" +
         "  \n" +
         "    {?bookText wdt:P31 wd:Q83790 . ?bookText wdt:P407 wd:Q1321 . ?bookText wdt:P577 ?fechaPublicadoLabel .  } #libro de texto\n" +
@@ -159,7 +87,125 @@ $(function() {
         "\n" +
         "\n" +
         "  \n" +
-        "  SERVICE wikibase:label { bd:serviceParam wikibase:language \"[ES],es\". }\n" +
+        "  SERVICE wikibase:label { bd:serviceParam wikibase:language \"[AUTO_LANGUAGE],es,en\". }\n" +
+        "}\n" +
+        "\n" +
+        "ORDER BY ASC(?bookTextLabel)\n" +
+        "",
+    settings = {
+        headers: { Accept: 'application/sparql-results+json' },
+        data: { query: sparqlQuery }
+    };
+
+$.ajax( endpointUrl, settings ).then( function ( data ) {
+  console.log(data.results.bindings);
+
+    if (data.results.bindings != null) {
+      for (var i in data.results.bindings) {
+        document.getElementById("libros").innerHTML += "<div class='tarjeta'>";
+
+        // console.log(data.results.bindings[i]);
+        if (data.results.bindings[i] != null) {
+          for (var j in data.results.bindings[i]) {
+            if (data.results.bindings[i][j] != null) {
+              // console.log("elemento I: "+data.results.bindings[i]);
+              // console.log("J: "+j+"Valor: "+data.results.bindings[i][j].value);
+
+
+              if (j=="image" && data.results.bindings[i].image != null) {
+                document.getElementById("libros").innerHTML += "<img class='portada' src='"+data.results.bindings[i].image.value+"'>";
+
+              }else if(data.results.bindings[i].image == null)  {
+                console.log("SIN IMAGEN");
+                console.log(data.results.bindings[i].bookTextLabel.value);
+                var portada = "img/default.png";
+                // document.getElementById("libros").innerHTML += "<img class='portada' src='"+portada+"'>";
+              }
+
+              if (j=="bookText" && data.results.bindings[i].bookText != null) {
+                document.getElementById("libros").innerHTML += "<h3><a target='_blank' href='"+data.results.bindings[i].bookText.value+"'</a>"+data.results.bindings[i].bookTextLabel.value+"</h3>";
+              }
+              if (j=="autor" && data.results.bindings[i].autor != null) {
+                document.getElementById("libros").innerHTML += "<h4><a target='_blank' href='"+data.results.bindings[i].autor.value+"'</a>"+data.results.bindings[i].autorLabel.value+"</h4>";
+              }
+              if (j=="genre" && data.results.bindings[i].genre != null) {
+                document.getElementById("libros").innerHTML += "<p><a target='_blank' href='"+data.results.bindings[i].genre.value+"'</a>"+MaysPrimera(data.results.bindings[i].genreLabel.value)+"</p>";
+              }
+              if (j=="idioma" && data.results.bindings[i].idioma != null) {
+                document.getElementById("libros").innerHTML += "<p><a target='_blank' href='"+data.results.bindings[i].idioma.value+"'</a>"+MaysPrimera(data.results.bindings[i].idiomaLabel.value)+"</p>";
+              }
+              if (j=="fechaPublicado" && data.results.bindings[i].fechaPublicado != null) {
+                var fecha = data.results.bindings[i].fechaPublicado.value.split("-")
+                document.getElementById("libros").innerHTML += "<p><small>"+fecha[0]+"</small></p>";
+              }
+              if (j=="bookTextDescription" && data.results.bindings[i].bookTextDescription != null) {
+                document.getElementById("libros").innerHTML += "<p><small>"+MaysPrimera(data.results.bindings[i].bookTextDescription.value)+"</small></p>";
+              }
+
+            }
+          }
+        }
+      }
+      document.getElementById("libros").innerHTML += "</div>";
+
+    }
+  } );
+}
+
+
+/* BUSCADOR*/
+$(function() {
+  var elemento = [];
+  // click ajax call
+  $("#searchTermAutor").keypress(function(e){
+    if(e.keyCode===13){
+      $.alert({
+      title: 'Estoy en ello!',
+      content: 'De momento solo click!',
+      });
+    }
+  });
+
+  $("#searchAutor").on("click", function() {
+    document.getElementById("cont").innerHTML = "";
+    var searchTermAutor = $("#searchTermAutor").val();
+    var searchTermAutorMAY= searchTermAutor.toUpperCase();
+
+    var endpointUrl = 'https://query.wikidata.org/sparql',
+    sparqlQuery = "SELECT DISTINCT ?bookText ?bookTextLabel ?bookTextDescription ?image ?autor ?autorLabel ?fechaPublicado ?genre ?genreLabel ?idioma ?idiomaLabel\n" +
+        "WHERE {\n" +
+        "  \n" +
+        "    {?bookText wdt:P31 wd:Q83790 . ?bookText wdt:P407 wd:Q1321 . ?bookText wdt:P577 ?fechaPublicadoLabel .  } #libro de texto\n" +
+        "   UNION\n" +
+        "    {?bookText wdt:P31 wd:Q571 . ?bookText wdt:P407 wd:Q1321 . ?bookText wdt:P577 ?fechaPublicado .} #libro impreso\n" +
+        "   UNION\n" +
+        "    {?bookText wdt:P31 wd:Q8261 . ?bookText wdt:P407 wd:Q1321 . ?bookText wdt:P577 ?fechaPublicado .} #novela\n" +
+        "   UNION\n" +
+        "    {?bookText wdt:P31 wd:Q35760 . ?bookText wdt:P407 wd:Q1321 . ?bookText wdt:P577 ?fechaPublicado .} #ensayo #idioma de la obra spaña\n" +
+        "   UNION\n" +
+        "    {?bookText wdt:P31 wd:Q37484 . ?bookText wdt:P407 wd:Q1321 . ?bookText wdt:P577 ?fechaPublicado .} #poema épico\n" +
+        "   UNION\n" +
+        "    {?bookText wdt:P31 wd:Q7725634 . ?bookText wdt:P407 wd:Q1321 . ?bookText wdt:P577 ?fechaPublicado .} #obra literaria Q5364419\n" +
+        "  UNION\n" +
+        "    {?bookText wdt:P31 wd:Q7725634 . ?bookText wdt:P407 wd:Q5364419 . ?bookText wdt:P577 ?fechaPublicado .} #obra literaria Español moderno\n" +
+        "   UNION\n" +
+        "    {?bookText wdt:P31 wd:Q25379 . ?bookText wdt:P407 wd:Q1321 . ?bookText wdt:P577 ?fechaPublicado .} #play\n" +
+        "   UNION\n" +
+        "    {?bookText wdt:P31 wd:Q25379 . ?bookText wdt:P407 wd:Q1088025 . ?bookText wdt:P577 ?fechaPublicado .} #play español antiguo\n" +
+        "  UNION\n" +
+        "    {?bookText wdt:P31 wd:Q25379 . ?bookText wdt:P407 wd:Q397 . ?bookText wdt:P577 ?fechaPublicado .} #play latin\n" +
+        "  \n" +
+        "  OPTIONAL {?bookText wdt:P577 ?fechaPublicado . ?bookText wdt:P18 ?image . ?bookText wdt:P50 ?autor . \n" +
+        "            ?bookText wdt:P136 ?genre . ?bookText wdt:P407 ?idioma . } \n" +
+        "\n" +
+        "\n" +
+        "  \n" +
+        "  #fecha del primer y último libro publicado\n" +
+        "  filter (?fechaPublicado > \"1492-01-01\"^^xsd:dateTime && ?fechaPublicado < \"1681-05-26\"^^xsd:dateTime) \n" +
+        "\n" +
+        "\n" +
+        "  \n" +
+        "  SERVICE wikibase:label { bd:serviceParam wikibase:language \"[AUTO_LANGUAGE],es,en\". }\n" +
         "}\n" +
         "\n" +
         "ORDER BY ASC(?bookTextLabel)\n" +
@@ -188,28 +234,6 @@ $(function() {
                       || j == "autorLabel" && data.results.bindings[i].autorLabel.value.toUpperCase().includes(searchTermAutorMAY)) {
                         elemento.push(data.results.bindings[i]);
                   }
-
-                  // if (j !=null && j == "bookTextLabel" && data.results.bindings[i][j].value.toUpperCase().includes(searchTermAutor)
-                  //     || j !=null && j == "autorLabel" && data.results.bindings[i][j].value.toUpperCase().includes(searchTermAutor)) {
-                  //
-                  //   document.getElementById("cont").innerHTML +="<h4>"+data.results.bindings[i].bookTextLabel.value+"</h4>";
-                  //
-                  //   if  (data.results.bindings[i].image !=null) {
-                  //     portada=data.results.bindings[i].image.value;
-                  //     document.getElementById("cont").innerHTML +="<img class='portada' src='"+portada+"'>";
-                  //
-                  //   }
-                  //
-                  //   if (data.results.bindings[i].autorLabel != null) {
-                  //     document.getElementById("cont").innerHTML +="<h5>"+data.results.bindings[i].autorLabel.value+"</h5>";
-                  //
-                  //   }
-                  //   if (data.results.bindings[i].genreLabel != null) {
-                  //     document.getElementById("cont").innerHTML +="<h6>"+data.results.bindings[i].genreLabel.value.toUpperCase()+"</h6>";
-                  //
-                  //   }
-                  //
-                  // }
                 }
               }
             }
@@ -218,9 +242,6 @@ $(function() {
       }
       // MOSTRAOS INFO de los libros resultantes
       // console.log(elemento);
-      function MaysPrimera(string){
-        return string.charAt(0).toUpperCase() + string.slice(1);
-      }
 
       if (elemento.length > 0) {
         for (var i in elemento) {
@@ -253,28 +274,24 @@ $(function() {
               var fecha = elemento[i].fechaPublicado.value.split("-")
               document.getElementById("cont").innerHTML += "<p><small>"+fecha[0]+"</small></p>";
             }
-
-
+            if (j=="bookTextDescription" && elemento[i].bookTextDescription != null) {
+              document.getElementById("cont").innerHTML += "<p><small>"+MaysPrimera(elemento[i].bookTextDescription.value)+"</small></p>";
+            }
 
           }
-
         }
         document.getElementById("cont").innerHTML += "</div>";
-
-
       }else {
         $.alert({
         title: 'No se han encontrado resultados!!!',
         content: 'Introduce un texto!',
         });
       }
-
-
       // vaciamos el array de libros
       while (elemento.length >0) {
         elemento.pop();
       }
-
     });
   });
+
 });
